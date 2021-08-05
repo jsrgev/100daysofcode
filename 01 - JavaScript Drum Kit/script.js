5db71c322fc6839436a9f1bd570403d0baafb62b
Josh Regev <jsrgev@gmail.com>
2021-08-05 17:03:25 +0300

    completed drum kit. added transitionEnd event and changed styling

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -3,7 +3,7 @@ const playsound = (e) => {
 	let sound = document.querySelector(`audio[data-key = ${e.key}]`);
 	let key = document.querySelector(`div[data-key=${e.key}]`);
 	key.classList.add("playing");
-	setTimeout(() =>{key.classList.remove("playing")},150);
+	// setTimeout(() =>{key.classList.remove("playing")},150);
 	if (!sound) {return};
 	// rewinds to start of audio, in case you hit key while it's still playing
 	sound.currentTime = 0;
@@ -11,3 +11,12 @@ const playsound = (e) => {
 }
 
 document.addEventListener('keydown',playsound);
+
+const removeTransition = (e) => {
+	if (e.propertyName !== 'transform') {return;}
+		console.log("run");
+	e.target.classList.remove('playing');
+}
+
+let keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend',removeTransition))
